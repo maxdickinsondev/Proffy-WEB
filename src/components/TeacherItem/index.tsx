2e51132b5c2123e04188080500ffd2d578051ef7
Max Dickinson <maxsantistadickinson@gmail.com>
2020-08-08 19:46:05 -0300

    Adicionando total de conexões.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import { createConnection } from 'net';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number,
@@ -19,6 +21,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
@@ -38,7 +46,12 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                         Preço/hora
                         <strong>R$ {teacher.cost} </strong>
                     </p>
-                    <a href={`https://wa.me/${teacher.whatsapp}`}>
+                    <a 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        onClick={createNewConnection} 
+                        href={`https://wa.me/${teacher.whatsapp}`}
+                    >
                         <img src={whatsappIcon} alt="Whatsapp" />
                         Entrar em contato
                     </a>
