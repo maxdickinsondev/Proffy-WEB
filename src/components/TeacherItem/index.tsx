653bd67dff15b785703e3cc3c018b79fcf637f66
Max Dickinson <maxsantistadickinson@gmail.com>
2020-08-08 19:41:03 -0300

    Listando professores disponíveis.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,33 +4,47 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-export default function TeacherItem() {
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: string,
+    name: string,
+    subject: string,
+    whatsapp: string,
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/59968647?s=460&u=81b334046950db301a9c5a3cb0fe9b264a00c8d9&v=4" alt="Max Dickinson"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Max Dickinson</strong>
-                        <span>React</span>
+                    <strong> {teacher.name} </strong>
+                    <span>{teacher.subject}</span>
                     </div>    
                 </header>
 
                 <p>
-                    Apaixonado por tecnologia.
-                    <br />
-                    Procuro estudar mais e mais a cada dia para resolver problemas e programar soluções nas tecnologias mais atuais do mercado.
+                    {teacher.bio}
                 </p>
                 
                 <footer>
                     <p>
                         Preço/hora
-                        <strong>R$ 80,00</strong>
+                        <strong>R$ {teacher.cost} </strong>
                     </p>
-                    <button type="button">
+                    <a href={`https://wa.me/${teacher.whatsapp}`}>
                         <img src={whatsappIcon} alt="Whatsapp" />
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
         </article>    
     );
 }
+
+export default TeacherItem;
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
@@ -12,6 +13,8 @@ import './styles.css';
 import api from '../../services/api';
 
 export default function TeacherForm() {
+    const history = useHistory();
+
     const [name, setName] = useState('');
     const [avatar, setAvatar] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
@@ -43,7 +46,7 @@ export default function TeacherForm() {
             cost: Number(cost),
             schedule: scheduleItems
         }).then(() => {
-            alert('Cadastro realizado com sucesso!');
+            history.push('/');
         }).catch(() => {
             alert('Erro no cadastro');
         });
